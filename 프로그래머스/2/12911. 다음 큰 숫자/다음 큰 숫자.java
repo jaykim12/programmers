07d515792ceaeb3cab576628db23df8adcabe7f3
김재형 <77326121+jaykim12@users.noreply.github.com>
2024-08-16 15:07:18 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,18 +1,6 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int bin_cnt = Integer.bitCount(n);
-        int bin_cnt2 = 0;
-
-        while (true){
-            n++;
-            bin_cnt2 = Integer.bitCount(n);
-            if(bin_cnt == bin_cnt2){
-                break;
-            }
-        }
-
-
-        return n;
+        return answer;
     }
 }
